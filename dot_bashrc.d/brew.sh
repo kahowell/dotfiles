3760e3939d7fc8e5c7685a2fb0b0acb80554186b
Kevin Howell <kevin@kahowell.net>
2022-12-10 14:54:48 -0500

    Add bash completion - brew packages, quarkus, crc

--- dot_bashrc.d/brew.sh
+++ dot_bashrc.d/brew.sh
@@ -3,3 +3,23 @@ if [ -f /home/linuxbrew/.linuxbrew/bin/brew ]; then
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
+COMPLETIONS="
+argocd
+brew
+chezmoi-completion.bash
+helm
+k3d
+k9s
+kind
+kubectl
+minikube
+oc
+starship
+stern
+yq
+"
+for file in $COMPLETIONS; do
+  if [ -f /home/linuxbrew/.linuxbrew/etc/bash_completion.d/$file ]; then
+    source /home/linuxbrew/.linuxbrew/etc/bash_completion.d/$file
+  fi
+done
---
+++ dot_bashrc.d/completion.sh
@@ -0,0 +1,9 @@
+COMMANDS="
+crc
+quarkus
+"
+for command in $COMMANDS; do
+  if [ -n "$(command -v command)" ]; then
+    source <($command completion bash)
+  fi
+done
