8a59860ce53b13852e2adab9606e312d78094fbf
Kevin Howell <kevin@kahowell.net>
2022-12-17 16:07:26 -0500

    Add android env vars

---
+++ dot_bashrc.d/android.sh
@@ -0,0 +1,2 @@
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/emulator"
