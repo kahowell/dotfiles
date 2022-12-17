67370a6fbcb330687d2b0a320974ae1e07dba02d
Kevin Howell <kevin@kahowell.net>
2022-12-17 17:32:00 -0500

    Put brew path after system path

--- dot_bashrc.d/brew.sh
+++ dot_bashrc.d/brew.sh
@@ -1,6 +1,9 @@
 # Set PATH, MANPATH, etc., for Homebrew.
-if [ -f /home/linuxbrew/.linuxbrew/bin/brew ]; then
+if [[ -f /home/linuxbrew/.linuxbrew/bin/brew && $PATH != *".linuxbrew"* ]]; then
+    OLDPATH=$PATH
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    # NOTE: unlike "idiomatic brew" I prefer system binaries when possible
+    export PATH=$OLDPATH:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin
 fi
 
 COMPLETIONS="
