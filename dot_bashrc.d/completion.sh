73d84f345bddd66744990d58d8ff05e85b124b1e
Kevin Howell <kevin@kahowell.net>
2023-12-23 11:53:58 -0500

    Fix quarkus completion

--- dot_bashrc.d/completion.sh
+++ dot_bashrc.d/completion.sh
@@ -1,9 +1,16 @@
 COMMANDS="
 crc
-quarkus
 "
 for command in $COMMANDS; do
   if [ -n "$(command -v $command)" ]; then
     source <($command completion bash)
+  fi
+done
+COMMANDS="
+quarkus
+"
+for command in $COMMANDS; do
+  if [ -n "$(command -v $command)" ]; then
+    source <($command completion)
   fi
 done
