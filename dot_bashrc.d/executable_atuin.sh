18bd65a1d9fa11e8dda2569936f996408a2bfb1b
Kevin Howell <khowell@redhat.com>
2024-08-29 15:31:49 -0400

    Disable atuin up-arrow

--- dot_bashrc.d/executable_atuin.sh
+++ dot_bashrc.d/executable_atuin.sh
@@ -1,4 +1,4 @@
 if [ -f /home/linuxbrew/.linuxbrew/etc/profile.d/bash-preexec.sh ]; then
   source /home/linuxbrew/.linuxbrew/etc/profile.d/bash-preexec.sh
-  eval "$(atuin init bash)"
+  eval "$(atuin init bash --disable-up-arrow)"
 fi
